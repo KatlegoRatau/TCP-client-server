cc32993d68d63a2a907beec21b081228831c602a
System Administrator <root@MacBook-Air.local>
2018-10-29 12:46:10 +0200

    Added Buffer

--- TServer.js
+++ TServer.js
@@ -1,26 +1,31 @@
 const net = require('net');
 const server = net.createServer();
+var Regex = require("regex");
+var regex = new Regex(/(a|b)*abb/);
+
 server.listen(4200);
 server.on('connection', socket => {
   console.log('Connected');
   
 
     let hasFound = false;
+
     socket.on("data",data =>{
         console.log(data.toString());
-     
-        for(let i = 0; i< data.toString().length; i++)
+
+       
+       let buffMsg = Buffer.from(data);
+       for(let i = 0; i< buffMsg.toString().length; i++)
         {
           
 
-          if(data.toString().charAt(i)=='/' && data.toString().charAt(i+1)=='>' )
+          if(buffMsg.toString().charAt(i)=='/' && buffMsg.toString().charAt(i+1)=='>' )
           {
              
-             //console.log(data.toString()[i].charAt(0));
-             //console.log(data.toString()[i+1].charAt(0));
-             let msg = data.toString().substring(0,i + 2);
+             console.log(buffMsg.toString()[i].charAt(0));
+             console.log(buffMsg.toString()[i+1].charAt(0));
+             let msg = buffMsg.toString().substring(0,i + 2);
              socket.write(msg);
-             
              socket.destroy();
 
           }
@@ -28,6 +33,28 @@ server.on('connection', socket => {
           
 
         }
+       
+        
+     
+        // for(let i = 0; i< data.toString().length; i++)
+        // {
+          
+
+        //   if(data.toString().charAt(i)=='/' && data.toString().charAt(i+1)=='>' )
+        //   {
+             
+        //      //console.log(data.toString()[i].charAt(0));
+        //      //console.log(data.toString()[i+1].charAt(0));
+        //      let msg = data.toString().substring(0,i + 2);
+        //      socket.write(msg);
+
+        //      socket.destroy();
+
+        //   }
+
+          
+
+        // }
 
          
           //console.log(Buffer.from(data));
